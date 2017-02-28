245b59bc71a3a15239a922171fd60932ac7d7bc2
Ryan Buccellato <rcb355@cornell.edu>
2017-02-28 02:19:37 -0500

    Accidentally removed abs function with last change, so I just changed that line.

--- DigitalPrototype/core/src/edu/cornell/gdiac/physics/platform/BulletModel.java
+++ DigitalPrototype/core/src/edu/cornell/gdiac/physics/platform/BulletModel.java
@@ -51,7 +51,7 @@ public class BulletModel extends BoxObstacle {
 
     public void update(float delta) {
         if(xtransform<maxXScale) {
-            xtransform += delta*speed*1.5f;
+            xtransform += delta*Math.abs(speed)*1.5f;
             this.setWidth(initWidth*xtransform);
             this.resize(getWidth(),getHeight());
             this.createFixtures();
