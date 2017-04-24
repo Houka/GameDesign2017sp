8f2d1f465f8ee583eef436576fcb1d9d6b386ea0
alexxu1 <ax35@cornell.edu>
2017-04-23 23:39:20 -0400

    Change snapping grid. Fix bug with splatterer directions.

--- MainGame/core/src/edu/cornell/gdiac/game/entity/controllers/CollisionController.java
+++ MainGame/core/src/edu/cornell/gdiac/game/entity/controllers/CollisionController.java
@@ -215,12 +215,11 @@ public class CollisionController implements ContactListener {
     }
     private void handleCollision(SplattererModel obj1, PaintballModel obj2) {
         boolean dir = false;
-        if(obj2.getX() < obj1.getX()) {
+        if(obj2.getVX() > 0) {
             dir = true;
         }
         if(!obj1.isUsed()) {
             obj2.instakill();
-
             obj1.setUsed(true);
             obj1.setShot(true);
             obj1.setDir(dir);
